d41f917048045e0d8eed19eea12f56ef9d97a344
Richard Wilkosz <richardwilkosz97@gmail.com>
2021-06-21 22:03:38 -0500

    App, Vuetify config

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
+    <router-view></router-view>
   </v-app>
 </template>
 
@@ -47,10 +8,32 @@
 import Vue from 'vue';
 
 export default Vue.extend({
-  name: 'App',
+  name: 'RichardResume',
 
   data: () => ({
     //
   }),
 });
 </script>
+
+<style lang="scss">
+// Add to div inside v-col you want to be sticky
+// Note: Make sure v-col element is "display: block"
+.StickyColContent {
+  position: sticky;
+  top: 0;
+  padding-top: 65px; // Prevent app bar overlap
+}
+
+// Makes footer spacing compatible with app-router
+.AppWithFooter {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+// Used for mobile browsers' scroll bouncing
+html {
+  background-color: #272727;
+}
+</style>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,20 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    // dark: true,
+    themes: {
+      light: {
+        primary: colors.teal.darken2,
+      },
+      dark: {
+        primary: colors.lightBlue.lighten3,
+        error: colors.red.lighten3,
+      }
+    }
+  }
 });
